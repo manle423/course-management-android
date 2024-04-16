d41f1f43268562a78ae850b5beaa84bb8f486432
manle423 <trungmanle.vm@gmail.com>
2024-04-16 17:50:10 +0700

    add route, service

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const host = "localhost";
+const route = require('./routes');
+
+//#region Routes
+route(app);
+//#endregion
+
+//#region Middleware
+
+//parse JSON
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+//handler error
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({
+    message: err.message,
+  });
+  return;
+});
+//#endregion
+
+app.listen(port, () => {
+  console.log(`App running on http://${host}:${port}`);
+});
---
+++ server/routes/index.js
@@ -0,0 +1,7 @@
+const courseRouter = require('./course');
+
+function route(app) {
+  app.use('/course', courseRouter);
+}
+
+module.exports = route;
